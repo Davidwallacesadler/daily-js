282224f309caaf300ba7f87e192016778cab284d
David Sadler <davidwallacesadler@gmail.com>
2020-12-01 09:59:42 -0700

    Work on problems to start off December
    December 1 - Two Sum. Start working on LeetCode questions.

---
+++ components/LargestProductOfThree.vue
@@ -0,0 +1,46 @@
+<template></template>
+
+<script>
+export default {
+  name: `LargestProductOfThree`,
+  props: {
+    list: {
+      type: Array,
+      default: () => [-10, -10, 5, 2],
+    },
+  },
+  computed: {
+    product() {
+      // need to take into account that the numbers could be negative - if so then we need one other neg
+      let product = 0
+      const listHasAtLeastTwoNegative = () => {
+        let negativeCount = 0
+        this.list.forEach((number) => {
+          if (number < 0) negativeCount += 1
+        })
+        if (negativeCount > 1) return true
+        else return false
+      }
+      const maxValue = Math.abs(Math.max(this.list))
+      const minValue = Math.abs(Math.max(this.list))
+      if (minValue > maxValue && listHasAtLeastTwoNegative) {
+        if (product === 0) {
+          // Need to start the product
+          product = minValue
+        }
+        product = product * minValue
+        // we have to use a negative number -- check if there exists another in the list
+      } else {
+        if (product === 0) {
+          product = maxValue
+        } else {
+          product = product * maxValue
+        }
+      }
+      return product
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ components/TwoSum.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>{{ nums }}</div>
+    <div>{{ target }}</div>
+  </div>
+</template>
+
+<script>
+// Given an array of integers (nums) and an integer (target)
+// return indicies of the two numbers that add up to the target.
+export default {
+  name: `TwoSum`,
+  props: {
+    nums: {
+      type: Array,
+      default: [2, 7, 11, 15],
+    },
+    target: {
+      type: Number,
+      default: 9,
+    },
+  },
+  computed: {
+    twoSum() {
+      const viableNumbers = this.nums.filter((number) => {
+        if (number <= this.target) return number
+      })
+      let firstNumberIndex = 0
+      let secondNumberIndex = 1
+      while (firstNumberIndex < viableNumbers.length) {
+        while (secondNumberIndex < viableNumbers.length) {
+          if (
+            viableNumbers[firstNumberIndex] +
+              viableNumbers[secondNumberIndex] !==
+            this.target
+          ) {
+            secondNumberIndex += 1
+          } else {
+            return [firstNumberIndex, secondNumberIndex]
+          }
+        }
+        firstNumberIndex += 1
+        secondNumberIndex = firstNumberIndex + 1
+      }
+    },
+  },
+}
+</script>
+
+<style></style>
